e6e82675df49722cf43602567be325ee516b17f4
rojan.panta <rojan.panta@swifttech.com.np>
2024-08-07 15:21:02 +0545

    feat: reviews-crud-complete

--- src/main/java/com/eCommerce/eCommerce/controller/ReviewController.java
+++ src/main/java/com/eCommerce/eCommerce/controller/ReviewController.java
@@ -6,22 +6,36 @@ import com.eCommerce.eCommerce.model.ApiResponse;
 import com.eCommerce.eCommerce.service.ReviewService;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("reviews")
+@PreAuthorize("hasRole('USER')")
 @AllArgsConstructor
 public class ReviewController {
 
     private final ReviewService reviewService;
 
     @PostMapping("/submit")
-    public ApiResponse submitReview(@Valid @RequestBody ReviewRequest reviewRequest, @AuthenticationPrincipal User user){
-        return  reviewService.submitReview(user.getId(), reviewRequest);
+    public ApiResponse submitReview(@Valid @RequestBody ReviewRequest reviewRequest, @AuthenticationPrincipal User user) {
+        return reviewService.submitReview(user.getId(), reviewRequest);
+    }
+
+    @DeleteMapping("/{id}/remove")
+    public ApiResponse removeReview(@PathVariable Long id, @AuthenticationPrincipal User user) {
+        return reviewService.removeReview(user.getId(), id);
+    }
+
+    @PutMapping("/{id}/update")
+    public ApiResponse updateReview(@PathVariable Long id, @RequestBody ReviewRequest reviewRequest, @AuthenticationPrincipal User user) {
+        return reviewService.updateReview(user.getId(), id, reviewRequest);
+    }
+
+    @GetMapping("/find")
+    public ApiResponse findReview(@AuthenticationPrincipal User user) {
+        return reviewService.findReviewByUser(user.getId());
     }
 
 }
--- src/main/java/com/eCommerce/eCommerce/service/impl/ReviewServiceImpl.java
+++ src/main/java/com/eCommerce/eCommerce/service/impl/ReviewServiceImpl.java
@@ -2,6 +2,7 @@ package com.eCommerce.eCommerce.service.impl;
 
 import com.eCommerce.eCommerce.builder.ResponseBuilder;
 import com.eCommerce.eCommerce.dto.ReviewRequest;
+import com.eCommerce.eCommerce.dto.response.ReviewResponse;
 import com.eCommerce.eCommerce.entity.Order;
 import com.eCommerce.eCommerce.entity.Product;
 import com.eCommerce.eCommerce.entity.Review;
@@ -14,6 +15,7 @@ import com.eCommerce.eCommerce.repository.ReviewRepository;
 import com.eCommerce.eCommerce.repository.UserRepository;
 import com.eCommerce.eCommerce.service.ReviewService;
 import lombok.AllArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -29,29 +31,77 @@ public class ReviewServiceImpl implements ReviewService {
     private final OrderRepository orderRepository;
 
     private final UserRepository userRepository;
+    private final ModelMapper modelMapper;
 
     @Override
-    public ApiResponse submitReview(Long userId, ReviewRequest request) {
+    public ApiResponse submitReview(Long userId, ReviewRequest reviewRequest) {
 
         Product product = productRepository
-                .findById(request.getProductId())
+                .findById(reviewRequest.getProductId())
                 .orElseThrow(() -> new EcommerceException("PRO001"));
-        List<Order> completedOrders = orderRepository.findByUserIdAndProductIdAndStatus(userId, request.getProductId(), OrderStatus.COMPLETED);
+        List<Order> completedOrders = orderRepository.findByUserIdAndProductIdAndStatus(userId, reviewRequest.getProductId(), OrderStatus.COMPLETED);
         if ((completedOrders.isEmpty())) {
-            throw
-                    new EcommerceException("ODR005");
+            throw new EcommerceException("ODR005");
+        }
+
+        boolean reviewExists = reviewRepository.existsByUserIdAndProductId(userId, reviewRequest.getProductId());
+        if (reviewExists) {
+            throw new EcommerceException("REV003");
         }
 
         Review review = new Review();
         review.setUser(userRepository.findById(userId).orElseThrow(() -> new EcommerceException("USR001")));
         review.setProduct(product);
-        review.setComment(request.getComment());
-        review.setRating(request.getRating());
+        review.setComment(reviewRequest.getComment());
+        review.setRating(reviewRequest.getRating());
 
         reviewRepository.save(review);
 
         return ResponseBuilder.buildSuccessResponse("message.review.submit.success");
 
+    }
+
+    @Override
+    public ApiResponse removeReview(Long userId, Long reviewId) {
+
+        Review review = reviewRepository.findById(reviewId).orElseThrow(() -> new EcommerceException("REV001"));
+
+        if (!review.getUser().getId().equals(userId)) {
+            throw new EcommerceException("REV002");
+        }
+        reviewRepository.delete(review);
+        return ResponseBuilder.buildSuccessResponse("message.review.delete.success");
+
+    }
+
+    @Override
+    public ApiResponse updateReview(Long userId, Long reviewId, ReviewRequest reviewRequest) {
+        Review review = reviewRepository.findById(reviewId).orElseThrow(() -> new EcommerceException("REV001"));
+
+        if (!review.getUser().getId().equals(userId)) {
+            throw new EcommerceException("REV002");
+        }
+
+        review.setComment(reviewRequest.getComment());
+        review.setRating(reviewRequest.getRating());
+        reviewRepository.save(review);
+
+        ReviewResponse reviewResponse = new ReviewResponse();
+        modelMapper.map(review, reviewResponse);
+
+
+        return ResponseBuilder.buildSuccessResponse(reviewResponse, "message.review.update.success");
+    }
+
+    @Override
+    public ApiResponse findReviewByUser(Long userId) {
+        List<Review> reviews = reviewRepository.findByUserId(userId);
+
+        List<ReviewResponse> reviewResponses = reviews.stream()
+                .map(review -> modelMapper.map(review, ReviewResponse.class))
+                .toList();
+
+        return ResponseBuilder.buildSuccessResponse(reviewResponses, "message.review.fetch.success");
     }
 
 }
